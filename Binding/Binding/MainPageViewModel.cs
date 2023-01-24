915031eb959c28c910be423d8d95063a6d724be3
Jens Kühner <jkuehner@web.de>
2023-01-24 11:21:27 +0100

    Simple Binding, INotifyPropertyChanged

--- Binding/Binding/MainPageViewModel.cs
+++ Binding/Binding/MainPageViewModel.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Binding
 {
-    internal class MainPageViewModel
+    internal class MainPageViewModel : INotifyPropertyChanged
     {
-        public string FullName { get; set; } = "Jens Kühner";
 
+        private string _fullName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FullName 
+        { get => _fullName;
+          set
+          {
+                if(_fullName != value)
+                {
+                    _fullName = value;
+                    NotifyPropertyChanged(nameof(FullName));
+                }
+          }
+        }
+
+        public int Counter { get; set; } = 1234;
+
+
+        public MainPageViewModel()
+        {
+            FullName = "Hallo Welt";
+        }
+
+        private void NotifyPropertyChanged(string propertyName) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
     }
 }
