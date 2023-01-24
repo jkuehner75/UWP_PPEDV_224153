eec8a3ce1280fc552c56892eeece25ce62b4d9c9
Jens Kühner <jkuehner@web.de>
2023-01-24 11:45:06 +0100

    Binding Converter

---
+++ Binding/Binding/ValidConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Binding
+{
+    internal class ValidConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if(value is bool b)
+            {
+                if (targetType == typeof(Brush))
+                {
+                    if (b)
+                        return new SolidColorBrush(Colors.Green);
+                    else
+                        return new SolidColorBrush(Colors.Red);
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
