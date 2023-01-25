567d6414fecd73d2bc5012d66f2f5c8a4f2d01c7
Jens Kühner <jkuehner@web.de>
2023-01-25 15:19:57 +0100

    Prism Unity MVVM Sample with one page. Skeleton.

---
+++ PrismMVVMStart/PrismMVVMStart/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Prism.Ioc;
+using Prism.Regions;
+using Prism.Unity;
+using PrismMVVMStart.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace PrismMVVMStart
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : PrismApplication
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override UIElement CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            var regionManager = Container.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(MainPage));
+
+
+            regionManager.RequestNavigate("ContentRegion", "MainPage");
+        }
+
+    }
+}
