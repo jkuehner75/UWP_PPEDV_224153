237980ee6ea4bf8dfce429e26ca4876215fd6691
Jens Kühner <jkuehner@web.de>
2023-01-25 10:06:15 +0100

    Custom Control & Button changes with Template

---
+++ TemplatedControl/MyControls/CustomControl1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace MyControls
+{
+    public sealed class CustomControl1 : Button
+    {
+        public CustomControl1()
+        {
+            this.DefaultStyleKey = typeof(CustomControl1);
+        }
+
+
+        
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(CustomControl1), new PropertyMetadata("Hallo"));
+
+
+    }
+}
