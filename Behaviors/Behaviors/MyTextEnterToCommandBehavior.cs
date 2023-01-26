90c7773833a0d26c84f963883e295d06c5366119
Jens Kühner <jkuehner@web.de>
2023-01-26 10:50:04 +0100

    Textbox Enter to command behavior with ICommand dp

---
+++ Behaviors/Behaviors/MyTextEnterToCommandBehavior.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace Behaviors
+{
+    public class MyTextEnterToCommandBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.KeyDown += AssociatedObject_KeyDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.KeyDown -= AssociatedObject_KeyDown;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObject_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                if(Command != null && Command.CanExecute(null))
+                    Command.Execute(null);
+            }
+        }
+
+
+
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Command.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(MyTextEnterToCommandBehavior), new PropertyMetadata(null));
+    }
+}
