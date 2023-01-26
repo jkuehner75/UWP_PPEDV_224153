52fe77d0a09fdbbc93888320318148b7c3db57a8
Jens Kühner <jkuehner@web.de>
2023-01-26 10:35:24 +0100

    Behavior previewKeydown numeric edit

---
+++ Behaviors/Behaviors/MyNumberTextBehavior.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xaml.Interactivity;
+using Windows.System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace Behaviors
+{
+    public class MyNumberTextBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewKeyDown += AssociatedObject_PreviewKeyDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewKeyDown -= AssociatedObject_PreviewKeyDown;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObject_PreviewKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if(e.Key < VirtualKey.Number0 || e.Key > VirtualKey.Number9)
+                e.Handled= true;
+        }
+    }
+}
